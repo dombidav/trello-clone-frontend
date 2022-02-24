38425c33e77376573b2216cae2ee61342c2d4c1e
Dombi Tibor <dombidav@gmail.com>
2022-02-24 09:48:16 +0100

    implemented storage service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core'
+
+import {Storage} from '@ionic/storage-angular'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  private _storage: Storage | null = null
+  private _ready = false
+
+  constructor(private readonly storage: Storage) {
+    this.init().then()
+  }
+
+  get ready(): boolean {
+    return this._ready
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    this._storage = await this.storage.create()
+    this._ready = true
+  }
+
+  async set(key: string, value: any) {
+    await this.waitForStorage()
+    await this._storage?.set(key, value)
+  }
+
+  async get<T = any>(key: string): Promise<T | null> {
+    await this.waitForStorage()
+    return (await this._storage?.get(key)) ?? null
+  }
+
+  async remove<T = any>(key: string): Promise<T | null> {
+    await this.waitForStorage()
+    return this._storage?.remove(key)
+  }
+
+  async waitForStorage() {
+    while (!this.ready) await new Promise((resolve) => setTimeout(resolve, 250))
+    return this._storage
+  }
+}
